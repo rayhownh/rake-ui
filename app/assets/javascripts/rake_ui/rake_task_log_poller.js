27c063a2120c7d94df1d8f9b5d015d661c52ef50
Ray Ho <rayho@squareup.com>
2025-05-09 12:36:32 -0700

    change interval time for log refresh

--- app/assets/javascripts/rake_ui/rake_task_log_poller.js
+++ app/assets/javascripts/rake_ui/rake_task_log_poller.js
@@ -1,43 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var logContentElement = document.getElementById('log_content');
-  
-  if (logContentElement) {
-    var contentUrl = logContentElement.getAttribute('data-content-url');
-    
-    if (contentUrl) {
-      var interval;
-      var isRequesting = false;
-      var hasAutoLoaded = false;
+var interval;
+    var content_url = "<%= @rake_task_log_content_url %>"
+    var is_requesting = false
 
-      function replaceContent(content) {
-        logContentElement.innerHTML = content;
-      }
-
-      interval = setInterval(function () {
-        if (isRequesting) { return; }
-        isRequesting = true;
-        fetch(contentUrl)
-          .then(function(r) { return r.json(); })
-          .then(function (r) {
-            replaceContent(r.rake_task_log_content);
-
-            if (r.is_rake_task_log_finished) {
-              clearInterval(interval);
-              
-              // Auto-load the log page when task is finished
-              if (!hasAutoLoaded) {
-                hasAutoLoaded = true;
-                window.location.reload(); // Reload the page to show the complete log
-              }
-            }
-            isRequesting = false;
-          })
-          .catch(function(err) {
-            isRequesting = false;
-            clearInterval(interval);
-            throw(err);
-          });
-      }, 500);
+    function replaceContent(content) {
+      document.getElementById('log_content').innerHTML = content
     }
-  }
-});
+
+    interval = setInterval(function () {
+      if (is_requesting) { return }
+      is_requesting = true;
+      fetch(content_url)
+        .then(function(r) { return r.json() })
+        .then(function (r) {
+          replaceContent(r.rake_task_log_content)
+
+          if (r.is_rake_task_log_finished) {
+            clearInterval(interval)
+          }
+          is_requesting = false
+        })
+        .catch(function(err) {
+          is_requesting = false
+          clearInterval(interval)
+          throw(err)
+        })
+    }, 500)
